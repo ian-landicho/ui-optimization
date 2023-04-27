7078249428f989181493d9589c73729d7914571b
Ian Landicho <landicho.ian@gmail.com>
2023-04-27 17:07:24 -0600

    Add memo limitations

--- src/app/with-memo.tsx
+++ src/app/with-memo.tsx
@@ -23,3 +23,23 @@ export default function WithMemo() {
 const MemoExpensiveComponent = React.memo(function MemoExpensiveComponent() {
   return <ExpensiveComponent />;
 });
+
+// If the component being wrapped contains useState, useReducer, or useContext,
+// then the component will still re-render if its state or context change
+const MemoExpensiveLimitation = React.memo(function MemoExpensiveLimitation() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div className="text-lg">
+      <button
+        type="button"
+        className="bg-gray-200 hover:bg-gray-300 active:bg-gray-400 px-2 py-1 rounded shadow-md uppercase"
+        onClick={() => setCount(c => c + 1)}
+      >
+        Expensive Increment
+      </button>
+      <div className="mt-6">Count: {count}</div>
+      <ExpensiveComponent />
+    </div>
+  );
+});
